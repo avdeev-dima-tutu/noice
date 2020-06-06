127395738293ff9baa9fb2b1184b2bddf842da8e
avdim <avdim@mail.ru>
2020-06-06 13:28:31 +0300

    assemble, install, run

--- assemble_install_run.sh
+++ assemble_install_run.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-#build
-./gradlew clean server:installDist app:assembleDebug app:assembleDebugAndroidTest --no-daemon
-APK="app/build/outputs/apk/debug/app-debug.apk"
-TEST_APK="app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk"
 
-#install
-adb install $APK
-adb install $TEST_APK
-
-#run
-./server/build/install/app/bin/app &
-adb shell am instrument -w -r -e class 'com.github.ashutoshgngwr.noice.fragment.AboutFragmentTest#testAboutItemClick' com.github.ashutoshgngwr.noice.debug.test/androidx.test.runner.AndroidJUnitRunner
-#https://developer.android.com/studio/test/command-line
-echo "end of script"
-kill %1
+./assemble.sh
+./install_run.sh
---
+++ install_run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#install
+APK="artifacts/app-debug.apk"
+TEST_APK="artifacts/app-debug-androidTest.apk"
+adb install $APK
+adb install $TEST_APK
+
+#run
+./server/build/install/app/bin/app &
+adb shell am instrument -w -r -e class 'com.github.ashutoshgngwr.noice.fragment.AboutFragmentTest#testAboutItemClick' com.github.ashutoshgngwr.noice.debug.test/androidx.test.runner.AndroidJUnitRunner
+#https://developer.android.com/studio/test/command-line
+echo "end of script"
+kill %1
