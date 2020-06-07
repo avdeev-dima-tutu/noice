c9852bf3d3c79404ae1f4a6f16c9128cde55b203
avdim <avdim@mail.ru>
2020-06-07 10:14:21 +0300

    8082

--- snapshot/src/main/java/com/test/ui_test_core/snapshot/todo_snapshot_record.kt
+++ snapshot/src/main/java/com/test/ui_test_core/snapshot/todo_snapshot_record.kt
@@ -10,7 +10,7 @@ fun assertSnapshot2(expect: ExpectSnapshotData, actual: Snapshot, description: S
         val modulePath = "app"//todo provide in gradle
 
         sendMultipart(
-            uploadServer = "http://10.0.2.2:9001",
+            uploadServer = "http://10.0.2.2:8082",
             name = "$modulePath/androidTest/resources/${expect.pngName}",
             fileBytes = actual.byteArray
         )
@@ -18,7 +18,7 @@ fun assertSnapshot2(expect: ExpectSnapshotData, actual: Snapshot, description: S
     assertSnapshot(expect, actual, description)
 }
 
-suspend fun sendMultipart(uploadServer: String = "http://127.0.0.1:9001", name:String = "file.bin", fileBytes: ByteArray = byteArrayOf(1, 2, 3, 4)): String {
+suspend fun sendMultipart(uploadServer: String = "http://127.0.0.1:8082", name:String = "file.bin", fileBytes: ByteArray = byteArrayOf(1, 2, 3, 4)): String {
     return HttpClient(Android).sendMultipart(
         uploadServer = uploadServer,
         name = name,
