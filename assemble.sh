79105c4b1efde1d7b76dd4506c57c6ba307ac43a
avdim <avdim@mail.ru>
2020-06-07 18:08:31 +0300

    ui_test_snhp-server

--- assemble.sh
+++ assemble.sh
@@ -2,9 +2,9 @@
 #build
 APP_MODULE="temp_app"
 BUILD_FLAVOR="" #todo
-./gradlew clean snapshot-server:installDist $APP_MODULE:assembleDebug $APP_MODULE:assembleDebugAndroidTest --no-daemon
+./gradlew clean ui_test_snhp-server:installDist $APP_MODULE:assembleDebug $APP_MODULE:assembleDebugAndroidTest --no-daemon
 mkdir build-artifacts
 mv temp_app/build/outputs/apk/debug/temp_app-debug.apk build-artifacts/
 mv temp_app/build/outputs/apk/androidTest/debug/temp_app-debug-androidTest.apk build-artifacts/
-cp -r snapshot-server/build/install/app build-artifacts/snapshot-server
+cp -r ui_test_snhp-server/build/install/app build-artifacts/ui_test_snhp-server
 
