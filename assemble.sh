eca4df738b30b840da5910876474629f490620ae
avdim <avdim@mail.ru>
2020-06-07 16:17:47 +0300

    snapshot-server

--- assemble.sh
+++ assemble.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #build
-./gradlew clean server:installDist temp_app:assembleDebug temp_app:assembleDebugAndroidTest --no-daemon
+APP_MODULE="temp_app"
+BUILD_FLAVOR="" #todo
+./gradlew clean snapshot-server:installDist $APP_MODULE:assembleDebug $APP_MODULE:assembleDebugAndroidTest --no-daemon
 mkdir build-artifacts
 mv temp_app/build/outputs/apk/debug/temp_app-debug.apk build-artifacts/
 mv temp_app/build/outputs/apk/androidTest/debug/temp_app-debug-androidTest.apk build-artifacts/
-cp -r server/build/install/app build-artifacts/snapshot-server
+cp -r snapshot-server/build/install/app build-artifacts/snapshot-server
 
