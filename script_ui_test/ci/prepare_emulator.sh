52b96db344940d0f000015aca382e6740ec2da64
avdim <avdim@mail.ru>
2020-06-08 22:48:35 +0300

    echo desciptions

--- script_ui_test/ci/prepare_emulator.sh
+++ script_ui_test/ci/prepare_emulator.sh
@@ -5,19 +5,42 @@ adb shell settings put global transition_animation_scale 0
 adb shell settings put global animator_duration_scale 0
 
 #Костыль для включения русского языка. Работате на девайсе -skin 1080x1920 -dpi-device 420 api 27
+
 sleep 2
-adb shell am start -a android.settings.LOCALE_SETTINGS # открываем системную настройку языка
+
+echo "открываем системную настройку языка"
+adb shell am start -a android.settings.LOCALE_SETTINGS
+
 sleep 2
-adb shell input tap 500 500 # Нажимаем кнопку add new language
+
+echo "Нажимаем кнопку add new language"
+adb shell input tap 500 500
+
 sleep 2
-adb shell input tap 1060 80 # Нажиамаем на лупу поиска
+
+echo "Нажиамаем на лупу поиска"
+adb shell input tap 1060 80
+
 sleep 2
-adb shell input text Russian # Вводим в поиск текст Russian
+
+echo "Вводим в поиск текст Russian"
+adb shell input text Russian
+
 sleep 2
-adb shell input tap 1 350 # Click on русский
+
+echo "Click on русский"
+adb shell input tap 1 350
+
 sleep 2
-adb shell input tap 1 400 # Click on Россия
+
+echo "Click on Россия"
+adb shell input tap 1 400
+
 sleep 2
-adb shell input swipe 1000 500 1000 300 500 # swipe russian language up
+
+echo "swipe russian language up"
+adb shell input swipe 1000 500 1000 300 500
+
 sleep 2
-#Всё, русский язык включен
+echo "Всё, русский язык включен"
+
