e4706e4dcaa8ad6e75812634bd546da84b4b9323
avdim <avdim@mail.ru>
2020-06-08 22:18:18 +0300

    move script to script_ui_test/ci dir

--- script_ui_test/ci/snapshot_install_run.sh
+++ script_ui_test/ci/snapshot_install_run.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-adb shell getprop
-./prepare_emulator.sh
-#install
+./script_ui_test/ci/prepare_emulator.sh
+adb shell getprop #print emulator props
+
+#install apk's
 APK="build-artifacts/ui_test_snapshot_sample_app-debug.apk"
 TEST_APK="build-artifacts/ui_test_snapshot_sample_app-debug-androidTest.apk"
 adb install $APK
 adb install $TEST_APK
 
-#run
+# Run snapshot server in background
 chmod -R 777 ./build-artifacts/ui_test_snapshot_server
-./build-artifacts/ui_test_snapshot_server/bin/app &
-#todo error if server not exists
+./build-artifacts/ui_test_snapshot_server/bin/app & #todo error if server not exists
+
+#run ui-tests
 adb shell am instrument -w -r -e class 'com.github.ashutoshgngwr.noice.fragment.AboutFragmentTest#testAboutItemClick' com.github.ashutoshgngwr.noice.debug.test/androidx.test.runner.AndroidJUnitRunner
 #https://developer.android.com/studio/test/command-line
 echo "end of script"
