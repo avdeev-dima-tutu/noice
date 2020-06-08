da84ee705407bf23ee337e839389af2a9bcf7a4c
avdim <avdim@mail.ru>
2020-06-08 18:04:46 +0300

    comments

--- prepare_emulator.sh
+++ prepare_emulator.sh
@@ -4,18 +4,20 @@ adb shell settings put global window_animation_scale 0
 adb shell settings put global transition_animation_scale 0
 adb shell settings put global animator_duration_scale 0
 
+#Костыль для включения русского языка. Работате на девайсе -skin 1080x1920 -dpi-device 420 api 27
 sleep 2
-adb shell am start -a android.settings.LOCALE_SETTINGS
+adb shell am start -a android.settings.LOCALE_SETTINGS # открываем системную настройку языка
 sleep 2
-adb shell input tap 500 500 # add new language
+adb shell input tap 500 500 # Нажимаем кнопку add new language
 sleep 2
-adb shell input tap 1060 80 #click on find
+adb shell input tap 1060 80 # Нажиамаем на лупу поиска
 sleep 2
-adb shell input text Russian
+adb shell input text Russian # Вводим в поиск текст Russian
 sleep 2
-adb shell input tap 1 350 # click on Russian #100 vars
+adb shell input tap 1 350 # Click on русский
 sleep 2
-adb shell input tap 1 400 # click on Россия
+adb shell input tap 1 400 # Click on Россия
 sleep 2
-adb shell input swipe 1000 500 1000 300 500 #move russian up
+adb shell input swipe 1000 500 1000 300 500 # swipe russian language up
 sleep 2
+#Всё, русский язык включен
